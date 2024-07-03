ca7c258420ac97ae3bd4d98cca434856ca37a4f8
Yuriy Volodin <vol1ura@gmail.com>
2024-07-03 19:44:13 +0300

    refactoring

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -12,5 +12,6 @@ class AthletesController < ApplicationController
 
     @command_results =
       Command.joins(:results).eager_load(:event).where(results: { athlete_id: @athlete.id }).order(date: :desc).distinct
+    @positions_count = @command_results.where(position: 1..3).unscope(:order).group(:position).count
   end
 end
